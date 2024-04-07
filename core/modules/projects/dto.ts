65640b0465a49bcd65988d588e6d360e0ebdd95a
Ubongabasi Jerome <87214555+dhelafrank@users.noreply.github.com>
2024-04-07 13:11:28 +0100

    Init: Projects DTO

---
+++ core/modules/projects/dto.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+
+const schema = Joi.object({
+  title: Joi.string().max(64).required(),
+  description: Joi.string().min(3).max(300).required(),
+  author: Joi.string().min(3).max(32).required(),
+  url: Joi.string().max(32).required(),
+  abstract: Joi.string().min(32).max(1512).required(),
+  dateSubmitted: Joi.object({
+    month: Joi.string().valid('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December').required(),
+    year: Joi.number().required()
+  }).required(),
+  academicYear: Joi.number().required(),
+  supervisors: Joi.array().items(Joi.string().max(64)).required(),
+  keywords: Joi.array().items(Joi.string().max(64)).required(),
+  departmentAcronym: Joi.string().required(),
+  ref: Joi.object({
+    Departments: Joi.string().required(),
+    Admins: Joi.string().required()
+  }).required(),
+  createdBy: Joi.string().required()
+});
+
+export default schema;
